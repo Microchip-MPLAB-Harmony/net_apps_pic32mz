85196e1cb00eb4bf4ef80b29718db532978fb8e2
Adrian Aur <Adrian.Aur@microchip.com>
2023-12-11 13:41:54 -0700

    [MH3-84379] - Increased the command buffer and stack size.

--- apps/web_ftp_server_mpfs_fatfs/firmware/src/config/pic32mz_ef_sk_freertos/configuration.h
+++ apps/web_ftp_server_mpfs_fatfs/firmware/src/config/pic32mz_ef_sk_freertos/configuration.h
@@ -96,11 +96,11 @@ extern "C" {
 
 #define SYS_CMD_ENABLE
 #define SYS_CMD_DEVICE_MAX_INSTANCES       SYS_CONSOLE_DEVICE_MAX_INSTANCES
-#define SYS_CMD_PRINT_BUFFER_SIZE          1024U
+#define SYS_CMD_PRINT_BUFFER_SIZE          2560U
 #define SYS_CMD_BUFFER_DMA_READY
 
 /* Command System Service RTOS Configurations*/
-#define SYS_CMD_RTOS_STACK_SIZE                1024
+#define SYS_CMD_RTOS_STACK_SIZE                2048
 #define SYS_CMD_RTOS_TASK_PRIORITY             1
 
 
@@ -234,12 +234,6 @@ extern "C" {
 
 
 
-/*** NBNS Configuration ***/
-#define TCPIP_STACK_USE_NBNS
-#define TCPIP_NBNS_TASK_TICK_RATE   110
-
-
-
 /*** HTTP NET Configuration ***/
 #define TCPIP_STACK_USE_HTTP_NET_SERVER
 #define TCPIP_HTTP_NET_MAX_HEADER_LEN		    		15
@@ -286,6 +280,12 @@ extern "C" {
 #define TCPIP_HTTP_NET_MALLOC_FUNC                  pvPortMalloc
 #define TCPIP_HTTP_NET_FREE_FUNC                    vPortFree
 #define TCPIP_HTTP_NET_CONSOLE_CMD           		false
+
+
+
+/*** NBNS Configuration ***/
+#define TCPIP_STACK_USE_NBNS
+#define TCPIP_NBNS_TASK_TICK_RATE   110
 
 
 /******************************************************************************/
