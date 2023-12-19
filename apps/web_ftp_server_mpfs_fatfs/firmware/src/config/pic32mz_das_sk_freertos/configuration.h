c5eb34b7fcce6afe700130dd580fd61e852c74d6
hrisikesh sahu <hrisikesh.sahu@microchip.com>
2023-12-19 18:45:34 +0530

    [MH3-84379] web_ftp_server_mpfs_fatfs application regeneration - pic32mz_das_sk_freertos

--- apps/web_ftp_server_mpfs_fatfs/firmware/src/config/pic32mz_das_sk_freertos/configuration.h
+++ apps/web_ftp_server_mpfs_fatfs/firmware/src/config/pic32mz_das_sk_freertos/configuration.h
@@ -237,12 +237,6 @@ extern "C" {
 
 
 
-/*** NBNS Configuration ***/
-#define TCPIP_STACK_USE_NBNS
-#define TCPIP_NBNS_TASK_TICK_RATE   110
-
-
-
 /*** HTTP NET Configuration ***/
 #define TCPIP_STACK_USE_HTTP_NET_SERVER
 #define TCPIP_HTTP_NET_MAX_HEADER_LEN		    		15
@@ -289,6 +283,12 @@ extern "C" {
 #define TCPIP_HTTP_NET_MALLOC_FUNC                  malloc
 #define TCPIP_HTTP_NET_FREE_FUNC                    free
 #define TCPIP_HTTP_NET_CONSOLE_CMD           		false
+
+
+
+/*** NBNS Configuration ***/
+#define TCPIP_STACK_USE_NBNS
+#define TCPIP_NBNS_TASK_TICK_RATE   110
 
 
 /******************************************************************************/
