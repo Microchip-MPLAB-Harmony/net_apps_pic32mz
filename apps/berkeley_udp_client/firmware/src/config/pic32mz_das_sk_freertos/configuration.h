823fba16c6a9e30530f16144602adf0f3e26afd1
Adrian Aur <Adrian.Aur@microchip.com>
2023-12-19 15:09:05 -0700

    [MH3-84275] - Regeneration of pic32mz_das_sk_freertos.X berkeley_udp_client demo

--- apps/berkeley_udp_client/firmware/src/config/pic32mz_das_sk_freertos/configuration.h
+++ apps/berkeley_udp_client/firmware/src/config/pic32mz_das_sk_freertos/configuration.h
@@ -321,7 +321,7 @@ extern "C" {
 
 /*** TCPIP Heap Configuration ***/
 #define TCPIP_STACK_USE_INTERNAL_HEAP
-#define TCPIP_STACK_DRAM_SIZE                       49250
+#define TCPIP_STACK_DRAM_SIZE                       54950
 #define TCPIP_STACK_DRAM_RUN_LIMIT                  2048
 
 #define TCPIP_STACK_MALLOC_FUNC                     malloc
