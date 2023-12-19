1ee17f645c1c3f9bf3d9a7bf8a700ed5f4a24c83
Mohammed Fahad R <mohammedfahad.r@microchip.com>
2023-12-19 19:14:50 +0530

    [MH3-84605] - Updated the SDL documentation with the addition of PPP demo documentation

---
+++ docs/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,4 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+define(function () {
+return {24:2,20:2,3:2,36:-1,19:36,1:2,0:2,32:2,5:2,35:2,30:2,2:-1,26:36};
+});
